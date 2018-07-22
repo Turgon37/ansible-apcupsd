1bfc9364cef84b602df14ad54b6b5627f886fb51
Pierre GINDRAUD <pgindraud@gmail.com>
2018-07-22 16:25:02 +0200

    typo in facts

--- files/apcupsd.fact.py
+++ files/apcupsd.fact.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
 import json
-import os
 import re
 import subprocess
 import sys
 
-content=dict()
+content = dict()
 
 # 6.6.1p1 style formatting
 # 7.2p2 style formatting
 version_re = re.compile('^apcupsd\s*(?P<version>(?P<major>[0-9]+)[^ ]+)')
 try:
-    result = subprocess.Popen(['/usr/bin/env', 'apcupsd', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    result = subprocess.Popen(['/usr/bin/env', 'apcupsd', '--version'],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              universal_newlines=True)
     (stdout, stderr) = result.communicate()
     match = version_re.search(stdout)
     if match:
